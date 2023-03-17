fe966f3d14ec62b9e19dab2f69f4044971cde458
Florian Wagner <slvwagner@gmail.com>
2023-03-17 01:49:03 +0100

    1. running version slvwagner

--- imGUIexample/UseImGui.h
+++ imGUIexample/UseImGui.h
@@ -4,6 +4,22 @@
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
 
+#include <fstream>
+#include <iostream>
+#include <cstdlib>
+//#include <unistd.h>
+
+#include <array>
+#include <vector>
+#include <list>
+#include <map>
+#include <stack>
+
+#include <algorithm>
+#include <math.h>
+#include <random>
+
+
 class UseImGui {
 public:
   void Init(GLFWwindow* window, const char* glsl_version);
