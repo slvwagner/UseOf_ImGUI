2fcb0b5dec732c4c44c352df9d5ab3b5332a531a
Florian Wagner <slvwagner@gmail.com>
2023-04-24 19:05:46 +0200

    wagnius logo includes with cmake

--- imGUIexample/imGUIexample.cpp
+++ imGUIexample/imGUIexample.cpp
@@ -145,7 +145,7 @@ int main()
 	CustomImGui myimgui;
 	myimgui.Init(window, glsl_version);
 	// Load image to display just once
-	myimgui.loadImage("D:/src_D/UseOf_ImGUI/imGUIexample/Picts/wagnius.png");
+	myimgui.loadImage("picts/wagnius.png");
 
 	//Setting up Webcam 
 	videoSettings();
